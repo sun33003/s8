a97a74ef35bb5779ef92a7a3821d36f50c435ec4
sun33003 <127114580+sun33003@users.noreply.github.com>
2026-03-03 10:51:30 +0800

    Update diymo.sh

--- ax6/diymo.sh
+++ ax6/diymo.sh
@@ -4,8 +4,6 @@ if [ ! -d "/var/run/homeproxy" ]; then
 fi
 
 #/etc/init.d/kodexplorer restart
-/etc/init.d/zerotier restart
 /etc/init.d/samba4 restart
 #/etc/init.d/ttyd restart
 /etc/init.d/homeproxy restart
-/etc/init.d/aria2 restart
